619b6c490adf720519fc6cbd4cf9ae4b7ba92516
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2006-11-13 12:09:46 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@27 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- avrupdate/firmware/main.c
+++ avrupdate/firmware/main.c
@@ -16,7 +16,7 @@
 /* external interrupt from usbn9604 */
 SIGNAL(SIG_INTERRUPT0)
 {
-  USBNInterrupt();
+  	USBNInterrupt();
 }
 
 uint8_t state;
@@ -43,9 +43,9 @@ void (*avrupdate_jump_to_app)( void ) = 0x0000;
 /*  wait function */
 void wait_ms(int ms)
 {
-  int i;
-  for(i=0;i<ms;i++)
-    _delay_ms(1);
+  	int i;
+  	for(i=0;i<ms;i++)
+    	_delay_ms(1);
 }
 
 /* pogramm a page into flash 
@@ -54,38 +54,38 @@ void wait_ms(int ms)
  */
 void avrupdate_program_page (uint32_t page)
 {
-  uint16_t i;
-  uint8_t sreg;
+  	uint16_t i;
+  	uint8_t sreg;
 
-  //SendHex(page);
-  page = page*128;
-  sreg = SREG;
-  cli();
+  	//SendHex(page);
+  	page = page*128;
+  	sreg = SREG;
+  	cli();
     
-  eeprom_busy_wait ();
+  	eeprom_busy_wait ();
 
-  boot_page_erase (page);
-  boot_spm_busy_wait ();      // Wait until the memory is erased.
-  uint8_t wbufaddr = 0;
+  	boot_page_erase (page);
+  	boot_spm_busy_wait ();      // Wait until the memory is erased.
+  	uint8_t wbufaddr = 0;
 
-  for (i=0; i<SPM_PAGESIZE; i+=2)
-  {
-    // Set up little-endian word.
-    uint16_t w = pageblock[wbufaddr++];
-    w += pageblock[wbufaddr++] << 8;
-    boot_page_fill (page + i, w);
-  }
+  	for (i=0; i<SPM_PAGESIZE; i+=2)
+  	{
+    	// Set up little-endian word.
+    	uint16_t w = pageblock[wbufaddr++];
+    	w += pageblock[wbufaddr++] << 8;
+    	boot_page_fill (page + i, w);
+  	}
 
-  boot_page_write (page);     // Store buffer in flash page.
-  boot_spm_busy_wait();       // Wait until the memory is written.
+  	boot_page_write (page);     // Store buffer in flash page.
+  	boot_spm_busy_wait();       // Wait until the memory is written.
 
-  // Reenable RWW-section again. We need this if we want to jump back
-  // to the application after bootloading.
+  	// Reenable RWW-section again. We need this if we want to jump back
+  	// to the application after bootloading.
 
-  boot_rww_enable ();
+  	boot_rww_enable ();
 
-  // Re-enable interrupts (if they were ever enabled).
-  SREG = sreg;
+  	// Re-enable interrupts (if they were ever enabled).
+  	SREG = sreg;
 }
 
 
@@ -93,107 +93,107 @@ void avrupdate_program_page (uint32_t page)
 // start programm from application sector
 void avrupdate_start_app()
 {
-  //UARTWrite("start\r\n");
-  if(collect128){
-    //SendHex(page_addr);
-    page_addr = page_addr/2;
-    avrupdate_program_page (page_addr);
-    UARTWrite("programm rest\r\n"); 
-  }
+  	//UARTWrite("start\r\n");
+  	if(collect128){
+    	//SendHex(page_addr);
+    	page_addr = page_addr/2;
+    	avrupdate_program_page (page_addr);
+    	UARTWrite("programm rest\r\n"); 
+  	}
 
-  USBNWrite(RXC1,FLUSH);
-  USBNWrite(RXC1,RX_EN);
+  	USBNWrite(RXC1,FLUSH);
+  	USBNWrite(RXC1,RX_EN);
 
-  USBNWrite(MCNTRL,SRST);  // clear all usb registers
+  	USBNWrite(MCNTRL,SRST);  // clear all usb registers
 
-  GICR = _BV(IVCE);  // enable wechsel der Interrupt Vectoren
-  GICR = 0x00; // Interrupts auf Application Section umschalten
+  	GICR = _BV(IVCE);  // enable wechsel der Interrupt Vectoren
+  	GICR = 0x00; // Interrupts auf Application Section umschalten
 
-  avrupdate_jump_to_app();	  // Jump to application sector
+  	avrupdate_jump_to_app();	  // Jump to application sector
 }
 
 
 void avrupdate_cmd(char *buf)
 {
-  int i;
-  // check state first ist 
-  switch(state)
-  {
-    case WRITEPAGE:
-      //UARTWrite("write\r\n");
-      //SendHex(page_addr);
+  	int i;
+  	// check state first ist 
+  	switch(state)
+  	{
+    	case WRITEPAGE:
+      		//UARTWrite("write\r\n");
+      		//SendHex(page_addr);
       
-      // if page sizte= 128 collect two 64 packages to a 128
-      if(SPM_PAGESIZE==0x80)
-      {
-	//UARTWrite("128\r\n");
-	//SendHex(page_addr);
-	if(page_addr%2)
-	{
-	  collect128=0;
-	  //UARTWrite("odd\r\n");
-	  // get sescond package
-	  if(page_addr==1)
-	    page_addr_w = 0;
-	  else 
-	    page_addr_w = (page_addr-1)/2;
+      		// if page sizte= 128 collect two 64 packages to a 128
+      		if(SPM_PAGESIZE==0x80)
+      		{
+				//UARTWrite("128\r\n");
+				//SendHex(page_addr);
+				if(page_addr%2)
+				{
+	  				collect128=0;
+	  				//UARTWrite("odd\r\n");
+	  				// get sescond package
+	  				if(page_addr==1)
+	    				page_addr_w = 0;
+	  				else 
+	    				page_addr_w = (page_addr-1)/2;
 	  
-	  for(i=0;i<64;i++)
-	  {
-	    pageblock[i+64]=buf[i];
-	  }
+	  				for(i=0;i<64;i++)
+	  				{
+	    				pageblock[i+64]=buf[i];
+	  				}
  
-	  // write page
-	  avrupdate_program_page (page_addr_w);
-	  state = NONE;
-	}else
-	{
-	  collect128=1;
-	  //UARTWrite("even\r\n");
-	  // get first package 
-	  for(i=0;i<64;i++)
-	    pageblock[i]=buf[i];
+	  				// write page
+	  				avrupdate_program_page (page_addr_w);
+	  				state = NONE;
+				}else
+				{
+	  				collect128=1;
+	  				//UARTWrite("even\r\n");
+	  				// get first package 
+	  				for(i=0;i<64;i++)
+	    				pageblock[i]=buf[i];
 
-	  state = NONE;
-	}	
-      } else
-      {
-	UARTWrite("64\r\n");
-	avrupdate_program_page (page_addr);
-	state = NONE;
-      }
-    break;
-    default:
-      //UARTWrite("default\r\n");
-      state = buf[0];
-      if(state==WRITEPAGE)
-	page_addr = buf[1];
+	  			state = NONE;
+				}	
+      		} else
+      		{
+				UARTWrite("64\r\n");
+				avrupdate_program_page (page_addr);
+				state = NONE;
+      		}
+    		break;
+    	default:
+      		//UARTWrite("default\r\n");
+      		state = buf[0];
+      		if(state==WRITEPAGE)
+				page_addr = buf[1];
 
-      if(state==STARTAPP)
-      {
-	//cli();
-	avrupdate_start_app();
-	state = NONE;
-      }
-
-  }
+      		if(state==STARTAPP)
+      		{
+				//cli();
+				avrupdate_start_app();
+				state = NONE;
+   			}	
+	}
 	
 }
 
 
 int main(void)
 {
-  // spm (bootloader mode from avr needs this, to use an own isr table)	
-  cli();
-  GICR = _BV(IVCE);  // enable wechsel der Interrupt Vectoren
-  GICR = _BV(IVSEL); // Interrupts auf Boot Section umschalten
-  sei();
+  	// spm (bootloader mode from avr needs this, to use an own isr table)	
+  	cli();
+  	GICR = _BV(IVCE);  // enable wechsel der Interrupt Vectoren
+  	GICR = _BV(IVSEL); // Interrupts auf Boot Section umschalten
+  	sei();
 
 
-  // bootloader application starts here
+  	// bootloader application starts here
 
-  const unsigned char avrupdateDevice[] =
-  { 0x12,	      // 18 length of device descriptor
+  	const unsigned char avrupdateDevice[] =
+  	{ 
+  	0x12,	      // 18 length of device descriptor
     0x01,       // descriptor type = device descriptor 
     0x10,0x01,  // version of usb spec. ( e.g. 1.1) 
     0x00,	      // device class
@@ -207,14 +207,15 @@ int main(void)
     0x00,	      // index of product string
     0x00,	      // index of ser. number
     0x01        // number of configs
-  };
+  	};
 
-  // ********************************************************************
-  // configuration descriptor          
-  // ********************************************************************
+  	// ********************************************************************
+  	// configuration descriptor          
+  	// ********************************************************************
 
-  const unsigned char avrupdateConf[] =
-  { 0x09,	      // 9 length of this descriptor
+  	const unsigned char avrupdateConf[] =
+  	{ 
+	0x09,	      // 9 length of this descriptor
     0x02,       // descriptor type = configuration descriptor 
     0x20,0x00,  // total length with first interface ... 
     0x01,	      // number of interfaces
@@ -246,30 +247,31 @@ int main(void)
     0x02,	      // transfer art ( bulk )
     0x00,0x08,  // fifo size
     0x00	      // polling intervall in ms
-  };
+  	};
 
-  UARTInit();
+  	UARTInit();
   
   
   
-  USBNInit(avrupdateDevice,avrupdateConf);   
-  USBNCallbackFIFORX1(&avrupdate_cmd);
+  	USBNInit(avrupdateDevice,avrupdateConf);   
+  	USBNCallbackFIFORX1(&avrupdate_cmd);
 
-  USBNInitMC();
+  	USBNInitMC();
 
-  // start usb chip
-  USBNStart();
+  	// start usb chip
+  	USBNStart();
   
-  UARTWrite("\r\nbootloader is now active\r\n");
+  	UARTWrite("\r\nbootloader is now active\r\n");
 
-  collect128=0;
-  // wait 2 seconds then start application
+  	collect128=0;
+  	// wait 2 seconds then start application
 
-  wait_ms(2000);
+  	while(1);
+	
+  	wait_ms(2000);
   
-  UARTWrite("\r\nbootloader start app now");
-  avrupdate_start_app();
+  	UARTWrite("\r\nbootloader start app now");
+  	avrupdate_start_app();
  
-  while(1);
 }
 
