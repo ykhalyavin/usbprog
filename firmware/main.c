8ebf9ace12b59b26d44b972e032925fcaec97a39
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2006-11-16 06:21:11 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@43 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- firmware/main.c
+++ firmware/main.c
@@ -3,11 +3,13 @@
 #include <stdint.h>
 #include <avr/interrupt.h>
 #include <inttypes.h>
+#include <util/delay.h>
 
 #include "uart.h"
 #include "usbn2mc.h"
 
-void (*avrupdate_jump_to_app)( void ) = 0x7000;
+//void (*avrupdate_jump_to_boot)( void ) = 0x7000;
+void (*avrupdate_jump_to_boot)( void ) = (void *) 0x7000;
 
 SIGNAL(SIG_UART_RECV)
 {
@@ -28,80 +30,84 @@ int togl=0;
 // testfunction where called when data on ep2, buf is a ptr to a 64 byte field 
 void Receive(char *buf)
 {
-  int i;
-  for(i=0;i<64;i++)
-    SendHex(buf[i]);
+  	int i;
+  	for(i=0;i<64;i++)
+    	SendHex(buf[i]);
 
-  UARTWrite("\r\nSend Back\r\n");
+  	UARTWrite("\r\nSend Back\r\n");
 
-  USBNWrite(TXC1,FLUSH);
-  for(i=0;i<64;i++)
-    USBNWrite(TXD1,i);
+  	USBNWrite(TXC1,FLUSH);
+  	for(i=0;i<64;i++)
+    	USBNWrite(TXD1,i);
 
-  USBNWrite(TXC1,TX_LAST+TX_EN);
+  	USBNWrite(TXC1,TX_LAST+TX_EN);
 
 }
 
 // called at transfer irq
 void TransferISR()
 {
-  //UARTWrite("ready for next\r\n");
-  int i;
+  	//UARTWrite("ready for next\r\n");
+  	int i;
 
-  USBNWrite(TXC1,FLUSH);
+  	USBNWrite(TXC1,FLUSH);
   
-  for(i=0;i<56;i++)
-    USBNWrite(TXD1,i);
+  	for(i=0;i<56;i++)
+    	USBNWrite(TXD1,i);
 
 
-  USBNWrite(TXC1,TX_LAST+TX_EN+TX_TOGL);
+  	USBNWrite(TXC1,TX_LAST+TX_EN+TX_TOGL);
 }
 
 
 int main(void)
 {
-  int conf, interf;
-  UARTInit();
-  USBNInit();   
-  // setup your usbn device
+  	int conf, interf;
+  	UARTInit();
+  	USBNInit();   
+  	// setup your usbn device
 
-  USBNDeviceVendorID(0x0400);
-  USBNDeviceProductID(0x9876);
-  USBNDeviceBCDDevice(0x0201);
+  	USBNDeviceVendorID(0x0400);
+  	USBNDeviceProductID(0x9876);
+  	USBNDeviceBCDDevice(0x0201);
 
 
-  char lang[]={0x09,0x04};
-  _USBNAddStringDescriptor(lang); // language descriptor
-
-  
-  USBNDeviceManufacture ("Benedikt Sauter <sauter@ixbat.de>");
-  USBNDeviceProduct	("usbprog USB Programmer");
-  USBNDeviceSerialNumber("200611121");
-
-  conf = USBNAddConfiguration();
-
-  //USBNConfigurationName(conf,"StandardKonfiguration");
-  USBNConfigurationPower(conf,50);
-
-  interf = USBNAddInterface(conf,0);
-  USBNAlternateSetting(conf,interf,0);
-
-  //USBNInterfaceName(conf,interf,"usbstorage");
-  
-
-  USBNAddOutEndpoint(conf,interf,1,0x02,BULK,64,0,&Receive);
-  USBNAddInEndpoint(conf,interf,1,0x03,BULK,64,0,&TransferISR);
+  	char lang[]={0x09,0x04};
+  	_USBNAddStringDescriptor(lang); // language descriptor
 
   
-  USBNInitMC();
-  sei();
+  	USBNDeviceManufacture ("Benedikt Sauter <sauter@ixbat.de>");
+  	USBNDeviceProduct	("usbprog USB Programmer");
+  	USBNDeviceSerialNumber("200611121");
 
-  // start usb chip
-  USBNStart();
-  //UARTWrite("waiting for enumaration signal...\r\n");
+  	conf = USBNAddConfiguration();
 
-  avrupdate_jump_to_app();      // Jump to application sector
-  while(1);
+  	//USBNConfigurationName(conf,"StandardKonfiguration");
+  	USBNConfigurationPower(conf,50);
+
+  	interf = USBNAddInterface(conf,0);
+  	USBNAlternateSetting(conf,interf,0);
+
+  	//USBNInterfaceName(conf,interf,"usbstorage");
+  
+
+  	USBNAddOutEndpoint(conf,interf,1,0x02,BULK,64,0,&Receive);
+  	USBNAddInEndpoint(conf,interf,1,0x03,BULK,64,0,&TransferISR);
+
+  
+  	USBNInitMC();
+  	sei();
+
+  	// start usb chip
+  	USBNStart();
+  	//UARTWrite("waiting for enumaration signal...\r\n");
+	
+	_delay_ms(2000);
+	/* mann muss hier noch sicherstellen, dass sicher der bootloader startet! */	
+	GICR |= _BV(IVSEL); //move interruptvectors to the Boot sector
+  	avrupdate_jump_to_boot();      // Jump to application sector
+
+  	while(1);
 }
 
 
