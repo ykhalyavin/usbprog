a5b019bb682bb68b8ca31ea06f45fcfde870cbb7
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2006-11-16 08:22:41 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@50 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- avrupdate/firmwarelib/avrupdate.h
+++ avrupdate/firmwarelib/avrupdate.h
@@ -1,5 +1,4 @@
 
-
 //void (*bootptr)( void ) = (void *) 0x0C00;
 void avrupdate_start();
 void avrupdate_check_start(char buf*);
--- avrupdate/lib/avrupdate.h
+++ avrupdate/lib/avrupdate.h
@@ -8,16 +8,21 @@ void avrupdate_close(struct usb_dev_handle* usb_handle);
 /* lowlevel funktions for avr */
 void avrupdate_flash_bin(struct usb_dev_handle* usb_handle,char *file);
 void avrupdate_startapp(struct usb_dev_handle* usb_handle);
-int avrupdate_get_uc_version();
+char avrupdate_get_version();
+void avrupdate_set_version(char version);
 
 /* check state of the controller -> ready for programming */
 int avrupdate_uc_state();
 
 
 /* online functions */
-void avrupdate_version_list(char * url);
+void avrupdate_net_version_list(char * url);
+void avrupdate_net_get_version();
+
+
+
+/* implicit functionst */
+
 void avrupdate_flash_version(int version);
-
-
 
 
