c11d2caea4a13b0a3b917d949e6df5924294aa9d
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2006-11-19 08:31:31 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@60 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- avrupdate/tools/terminal/main.c
+++ avrupdate/tools/terminal/main.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 #include <usb.h>
-//#include "avrupdate.h"
+#include "avrupdate.h"
 
 
 int main(int argc, char **argv)
 {
-
-  // start with the destination of your bin file
+	/*
+  	// start with the destination of your bin file
 	
-  struct usb_dev_handle* usb_handle;
-  usb_handle = avrupdate_open(0x0400,0x5dc3);
+  	struct usb_dev_handle* usb_handle;
+  	usb_handle = avrupdate_open(0x0400,0x5dc3);
  
-  if(argc==2)
-    avrupdate_flash_bin(usb_handle,argv[1]);
+  	if(argc==2)
+    	avrupdate_flash_bin(usb_handle,argv[1]);
 
 
-  avrupdate_startapp(usb_handle);
-  avrupdate_close(usb_handle);
+  	avrupdate_startapp(usb_handle);
+  	avrupdate_close(usb_handle);
+	*/
 
-  return 0;
+	const char *url = "http://www.ixbat.de/versions.conf";
+	printf("Found %i version(s)\n",avrupdate_net_versions(url));	
+
+	struct avrupdate_info *tmp = avrupdate_net_get_version_info(url,1);
+
+	printf("titel %s\n",tmp->title);
+	printf("version %s\n",tmp->version);
+	printf("file %s\n",tmp->file);
+	printf("description %s\n",tmp->description);
+
+
+	avrupdate_net_flash_version(url,0);
+	
+
+  	return 0;
 }
