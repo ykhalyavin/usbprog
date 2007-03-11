916df0c2d48b5c1f780916fd887f596042034d3b
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2007-03-11 19:23:06 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@300 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- usbprog_base/firmwarelib/avrupdate.c
+++ usbprog_base/firmwarelib/avrupdate.c
@@ -7,12 +7,12 @@ uint8_t ee_version EEMEM = 1;
 
 void avrupdate_start()
 {
-	GICR |= _BV(IVSEL); //move interruptvectors to the Boot sector
 	eeprom_write_byte(&ee_version,0xFF); //force bootloader to start
 
- uint8_t myByte;
- while(myByte !=0xFF)
- 	myByte = eeprom_read_byte(&ee_version);
+  uint8_t myByte;
+  while(myByte !=0xFF)
+ 	 myByte = eeprom_read_byte(&ee_version);
 
+	GICR |= _BV(IVSEL); //move interruptvectors to the Boot sector
 	avrupdate_jump_to_boot();
 }
