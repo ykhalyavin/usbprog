b17fe81d0ebf19dc84563bb3b747fb65c3168ad0
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2007-04-25 13:07:11 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@398 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- simpleport/firmware/simpleport.c
+++ simpleport/firmware/simpleport.c
@@ -16,35 +16,74 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-
 #include "simpleport.h"
 
 void set_direction(uint8_t direction)
 {
-
+  // 0 = input, 1 = output
+  if(direction && 0x01) SETBIT(BIT0_DDR,BIT0); else CLEARBIT(BIT0_DDR,BIT0);
+  if(direction && 0x02) SETBIT(BIT1_DDR,BIT1); else CLEARBIT(BIT1_DDR,BIT1);
+  if(direction && 0x04) SETBIT(BIT2_DDR,BIT2); else CLEARBIT(BIT2_DDR,BIT2);
+  if(direction && 0x08) SETBIT(BIT3_DDR,BIT3); else CLEARBIT(BIT3_DDR,BIT3);
+  if(direction && 0x10) SETBIT(BIT4_DDR,BIT4); else CLEARBIT(BIT4_DDR,BIT4);
+  if(direction && 0x20) SETBIT(BIT5_DDR,BIT5); else CLEARBIT(BIT5_DDR,BIT5);
+  if(direction && 0x40) SETBIT(BIT6_DDR,BIT6); else CLEARBIT(BIT6_DDR,BIT6);
 }
 
 
 void set_port(uint8_t value)
 {
+  // BIT0 - BIT 3
+  BIT0_WRITE = 0x0F & value;
+
+  //BIT4 - BIT 5
+  if(value && 0x10) SETBIT(BIT4_WRITE,BIT4); else CLEARBIT(BIT4_WRITE,BIT4);
+  if(value && 0x20) SETBIT(BIT5_WRITE,BIT5); else CLEARBIT(BIT5_WRITE,BIT5);
+
+  // BIT 6
+  if(value && 0x40) SETBIT(BIT6_WRITE,BIT6); else CLEARBIT(BIT6_WRITE,BIT6);
 
 }
 
 uint8_t get_port()
 {
-
+  uint8_t result=0x00; 
+  if(IS_BIT0_SET()) result |= (1<<BIT0);
+  if(IS_BIT1_SET()) result |= (1<<BIT1);
+  if(IS_BIT2_SET()) result |= (1<<BIT2);
+  if(IS_BIT3_SET()) result |= (1<<BIT3);
+  if(IS_BIT4_SET()) result |= (1<<BIT4);
+  if(IS_BIT5_SET()) result |= (1<<BIT5);
+  if(IS_BIT6_SET()) result |= (1<<BIT6);
+  return result;
 }
 
 
 void set_bit(uint8_t bit, uint8_t value)
 {
+  switch(bit) {
+    case 0: if(value==1) SETBIT(BIT0_WRITE,BIT0); else CLEARBIT(BIT0_WRITE,BIT0); break;
+    case 1: if(value==1) SETBIT(BIT1_WRITE,BIT1); else CLEARBIT(BIT1_WRITE,BIT1); break;
+    case 2: if(value==1) SETBIT(BIT2_WRITE,BIT2); else CLEARBIT(BIT2_WRITE,BIT2); break;
+    case 3: if(value==1) SETBIT(BIT3_WRITE,BIT3); else CLEARBIT(BIT3_WRITE,BIT3); break;
+    case 4: if(value==1) SETBIT(BIT4_WRITE,BIT4); else CLEARBIT(BIT4_WRITE,BIT4); break;
+    case 5: if(value==1) SETBIT(BIT5_WRITE,BIT5); else CLEARBIT(BIT5_WRITE,BIT5); break;
+    case 6: if(value==1) SETBIT(BIT5_WRITE,BIT6); else CLEARBIT(BIT6_WRITE,BIT6); break;
+  }
 
 }
 
 
-void get_get(uint8_t bit)
+uint8_t get_get(uint8_t bit)
 {
-
-
+  switch(bit) {
+    case 0: return IS_BIT0_SET(); break;
+    case 1: return IS_BIT1_SET(); break;
+    case 2: return IS_BIT2_SET(); break;
+    case 3: return IS_BIT3_SET(); break;
+    case 4: return IS_BIT4_SET(); break;
+    case 5: return IS_BIT5_SET(); break;
+    case 6: return IS_BIT6_SET(); break;
+  }
 }
 
--- simpleport/firmware/simpleport.h
+++ simpleport/firmware/simpleport.h
@@ -16,62 +16,88 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "simpleport.h"
 
 #define BIT(x)            (1 << (x))
 #define SETBIT(addr,x)    (addr |= BIT(x))
 #define CLEARBIT(addr,x)  (addr &= ~BIT(x))
 
+#define BIT0	    PB5
+#define BIT1  	    PB0
+#define BIT2	    PB7
+#define BIT3	    PB6
 
-#define JTAG_PORT_INIT          DDRB
-#define JTAG_PORT_WRITE         PORTB
-#define JTAG_PORT_READ          PINB
+#define BIT4	    PD0
+#define BIT5	    PD1
 
-#define TDI	    PB5
-#define TDO	    PB6
-#define TMS	    PB0
-#define TCK	    PB7
+#define BIT6	    PA4
 
-#define LED_PIN     PA4
-#define LED_PORT    PORTA
 
-#define LED_ON()     (LED_PORT   |=  (1 << LED_PIN))   // red led
-#define LED_OFF()    (LED_PORT   &= ~(1 << LED_PIN))
+#define BIT0_DDR    DDRB
+#define BIT1_DDR    DDRB
+#define BIT2_DDR    DDRB
+#define BIT3_DDR    DDRB
 
+#define BIT4_DDR    DDRD
+#define BIT5_DDR    DDRD
+#define BIT6_DDR    DDRA
+
+
+#define BIT0_READ   PINB
+#define BIT1_READ   PINB
+#define BIT2_READ   PINB
+#define BIT3_READ   PINB
+
+#define BIT4_READ   PIND
+#define BIT5_READ   PIND
+#define BIT6_READ   PINA
+
+
+#define BIT0_WRITE   PORTB
+#define BIT1_WRITE   PORTB
+#define BIT2_WRITE   PORTB
+#define BIT3_WRITE   PORTB
+
+#define BIT4_WRITE   PORTD
+#define BIT5_WRITE   PORTD
+#define BIT6_WRITE   PORTA
 
 // check if tdo == 1
-#define JTAG_IS_TDO_SET()		    (JTAG_PORT_READ & BIT(TDO))
+#define IS_BIT0_SET()			    (BIT0_READ & BIT(BIT0))
+#define IS_BIT1_SET()			    (BIT1_READ & BIT(BIT1))
+#define IS_BIT2_SET()			    (BIT2_READ & BIT(BIT2))
+#define IS_BIT3_SET()			    (BIT3_READ & BIT(BIT3))
 
-#define JTAG_SET_TCK()                       SETBIT( JTAG_PORT_WRITE, TCK )
-#define JTAG_CLEAR_TCK()                     CLEARBIT( JTAG_PORT_WRITE, TCK )
-//
-#define JTAG_SET_TMS()                       SETBIT( JTAG_PORT_WRITE, TMS )
-#define JTAG_CLEAR_TMS()                     CLEARBIT( JTAG_PORT_WRITE, TMS )
-
-#define JTAG_SET_TDI()                       SETBIT( JTAG_PORT_WRITE, TDI )
-#define JTAG_CLEAR_TDI()                     CLEARBIT( JTAG_PORT_WRITE, TDI )
-
-#define JTAG_SET_TCK()                       SETBIT( JTAG_PORT_WRITE, TCK )
-#define JTAG_CLEAR_TCK()                     CLEARBIT( JTAG_PORT_WRITE, TCK )
+#define IS_BIT4_SET()			    (BIT4_READ & BIT(BIT4))
+#define IS_BIT5_SET()			    (BIT5_READ & BIT(BIT5))
+#define IS_BIT6_SET()			    (BIT6_READ & BIT(BIT6))
 
 
-    // start usb chip
-    USBNStart();
+#define SET_BIT0()			     SETBIT( BIT0_WRITE, BIT0 )
+#define SET_BIT1()			     SETBIT( BIT1_WRITE, BIT1 )
+#define SET_BIT2()			     SETBIT( BIT2_WRITE, BIT2 )
+#define SET_BIT3()			     SETBIT( BIT3_WRITE, BIT3 )
 
-    // led
-    DDRA = (1 << PA4);
+#define SET_BIT4()			     SETBIT( BIT4_WRITE, BIT4 )
+#define SET_BIT5()			     SETBIT( BIT5_WRITE, BIT5 )
+#define SET_BIT6()			     SETBIT( BIT6_WRITE, BIT6 )
+
+#define CLEAR_BIT0()			     CLEARBIT( BIT0_WRITE, BIT0 )
+#define CLEAR_BIT1()			     CLEARBIT( BIT1_WRITE, BIT1 )
+#define CLEAR_BIT2()			     CLEARBIT( BIT2_WRITE, BIT2 )
+#define CLEAR_BIT3()			     CLEARBIT( BIT3_WRITE, BIT3 )
+
+#define CLEAR_BIT4()			     CLEARBIT( BIT4_WRITE, BIT4 )
+#define CLEAR_BIT5()			     CLEARBIT( BIT5_WRITE, BIT5 )
+#define CLEAR_BIT6()			     CLEARBIT( BIT6_WRITE, BIT6 )
 
 
-    // use as output
-    JTAG_PORT_INIT |= (1<<TCK)|(1<<TMS)|(1<<TDI);
-    // use as input
-    JTAG_PORT_INIT &=~(1<<TDO);
-    // pullup
-    JTAG_PORT_WRITE |= (1<<TDO);
+
 
 
 void set_direction(uint8_t direction);
 void set_port(uint8_t value);
 uint8_t get_port();
 void set_bit(uint8_t bit, uint8_t value);
-void get_get(uint8_t bit);
+uint8_t get_get(uint8_t bit);
 
