8a9a2ffa05af6a8a12828f8054c2d01f7bb8a977
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2007-04-27 17:22:14 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@417 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- openocd/firmware/lib/jtagusb.c
+++ openocd/firmware/lib/jtagusb.c
@@ -15,20 +15,20 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-#include "simpleport.h"
+#include "usbprog_jtag.h"
 
 #include <usb.h>
 
-struct simpleport* simpleport_open()
+struct usbprog_jtag* usbprog_jtag_open()
 {
   struct usb_bus *busses;
   struct usb_dev_handle* usb_handle;
   struct usb_bus *bus;
   struct usb_device *dev;
 
-  struct simpleport * tmp;
+  struct usbprog_jtag * tmp;
 
-  tmp = (struct simpleport*)malloc(sizeof(struct simpleport));
+  tmp = (struct usbprog_jtag*)malloc(sizeof(struct usbprog_jtag));
 
 
   usb_init();
@@ -37,19 +37,19 @@ struct simpleport* simpleport_open()
 
   busses = usb_get_busses();
 
-  /* find simpleport device in usb bus */
+  /* find usbprog_jtag device in usb bus */
 
   for (bus = busses; bus; bus = bus->next){
     for (dev = bus->devices; dev; dev = dev->next){
       /* condition for sucessfully hit (too bad, I only check the vendor id)*/
       if (dev->descriptor.idVendor == VID && dev->descriptor.idProduct == PID) {
-	tmp->usb_handle = usb_open(dev);
+				tmp->usb_handle = usb_open(dev);
 
-	usb_set_configuration (tmp->usb_handle,dev->config[0].bConfigurationValue);
-	usb_claim_interface(tmp->usb_handle, 0);
-	usb_set_altinterface(tmp->usb_handle,0);
+				usb_set_configuration (tmp->usb_handle,dev->config[0].bConfigurationValue);
+				usb_claim_interface(tmp->usb_handle, 0);
+				usb_set_altinterface(tmp->usb_handle,0);
 
-	return tmp;
+				return tmp;
       }
     } 
   }
@@ -57,18 +57,18 @@ struct simpleport* simpleport_open()
 }
 
 
-void simpleport_close(struct simpleport *simpleport)
+void usbprog_jtag_close(struct usbprog_jtag *usbprog_jtag)
 {
-  usb_close(simpleport->usb_handle);
-  free(simpleport);
+  usb_close(usbprog_jtag->usb_handle);
+  free(usbprog_jtag);
 }
 
 
-unsigned char simpleport_message(struct simpleport *simpleport, char *msg, int msglen)
+unsigned char usbprog_jtag_message(struct usbprog_jtag *usbprog_jtag, char *msg, int msglen)
 {
-  int res = usb_bulk_write(simpleport->usb_handle,3,msg,msglen,100);
+  int res = usb_bulk_write(usbprog_jtag->usb_handle,3,msg,msglen,100);
   if(res == msglen) {
-    res =  usb_bulk_read(simpleport->usb_handle,2, msg, 2, 100);
+    res =  usb_bulk_read(usbprog_jtag->usb_handle,2, msg, 2, 100);
     if (res > 0)
       return (unsigned char)msg[1];
     else 
@@ -78,33 +78,33 @@ unsigned char simpleport_message(struct simpleport *simpleport, char *msg, int m
     return -1;
 }
 
-void simpleport_set_direction(struct simpleport *simpleport, unsigned char direction)
+void usbprog_jtag_set_direction(struct usbprog_jtag *usbprog_jtag, unsigned char direction)
 {
   char tmp[2];
   tmp[0] = PORT_DIRECTION;
   tmp[1] = (char)direction;
-  simpleport_message(simpleport,tmp,2);
+  usbprog_jtag_message(usbprog_jtag,tmp,2);
 }
 
-void simpleport_set_port(struct simpleport *simpleport,unsigned char value, unsigned char mask)
+void usbprog_jtag_write_slice(struct usbprog_jtag *usbprog_jtag,unsigned char value)
 {
   char tmp[3];
   tmp[0] = PORT_SET;
   tmp[1] = (char)value;
   tmp[2] = (char)mask;
-  simpleport_message(simpleport,tmp,3);
+  usbprog_jtag_message(usbprog_jtag,tmp,3);
 }
 
-unsigned char simpleport_get_port(struct simpleport *simpleport)
+unsigned char usbprog_jtag_get_port(struct usbprog_jtag *usbprog_jtag)
 {
   char tmp[2];
   tmp[0] = PORT_GET;
   tmp[1] = 0x00;
-  return simpleport_message(simpleport,tmp,2);
+  return usbprog_jtag_message(usbprog_jtag,tmp,2);
 }
 
 
-void simpleport_set_bit(struct simpleport *simpleport,int bit, int value)
+void usbprog_jtag_set_bit(struct usbprog_jtag *usbprog_jtag,int bit, int value)
 {
   char tmp[3];
   tmp[0] = PORT_SETBIT;
@@ -113,16 +113,16 @@ void simpleport_set_bit(struct simpleport *simpleport,int bit, int value)
     tmp[2] = 0x01;
   else
     tmp[2] = 0x00;
-  simpleport_message(simpleport,tmp,3);
+  usbprog_jtag_message(usbprog_jtag,tmp,3);
 }
 
-int simpleport_get_bit(struct simpleport *simpleport, int bit)
+int usbprog_jtag_get_bit(struct usbprog_jtag *usbprog_jtag, int bit)
 {
   char tmp[2];
   tmp[0] = PORT_GETBIT;
   tmp[1] = (char)bit;
 
-  if(simpleport_message(simpleport,tmp,2)>0)
+  if(usbprog_jtag_message(usbprog_jtag,tmp,2)>0)
     return 1;
   else
     return 0;
