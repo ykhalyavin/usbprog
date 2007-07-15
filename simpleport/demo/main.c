e72ea3650437220038755f80483fcf589af6a6fa
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2007-07-15 18:29:49 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@445 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- simpleport/demo/main.c
+++ simpleport/demo/main.c
@@ -14,14 +14,17 @@ int main()
   if(sp_handle==0)
     fprintf(stderr,"unable to open device\n");
 
-
-  simpleport_set_direction(sp_handle,0xFE);
+  simpleport_set_direction(sp_handle,0xFF);
 
   while(1){
     //simpleport_set_port(sp_handle,0x80);
     //simpleport_set_port(sp_handle,0x00);
-    simpleport_set_bit(sp_handle,4,1);
-    simpleport_set_bit(sp_handle,4,0);
+    //simpleport_set_bit(sp_handle,6,1);
+    //simpleport_set_bit(sp_handle,6,0);
+    simpleport_set_port(sp_handle,0xFF,0xFF);
+    sleep(1);
+    simpleport_set_port(sp_handle,0x00,0xFF);
+    sleep(1);
   }
 
 /*
