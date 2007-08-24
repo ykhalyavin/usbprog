b38137d875162fdceb66a5a405fcb0076384d37f
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2007-08-24 15:12:58 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@666 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- simpleport/examples/example.py
+++ simpleport/examples/example.py
@@ -8,19 +8,21 @@
 # author: Michael Kaiser
 #
 
-import simpleport
+import sys
 import time
 
 if __name__ == "__main__":
-
+    sys.path.append('../lib')
+    import simpleport
     # call simpleport_open() to retrive a handle
     sp_handle = simpleport.simpleport_open()
+    simpleport.simpleport_set_pin_dir(sp_handle, 11, 1)
 
     # periodacally set entire port to '11111111' and '00000000'
     while 1:
-	simpleport.simpleport_set_port(sp_handle, 0xFF, 0xFF)
+	simpleport.simpleport_set_pin(sp_handle, 11, 1)
 	time.sleep(1)
-	simpleport.simpleport_set_port(sp_handle, 0x00, 0xFF)
+	simpleport.simpleport_set_pin(sp_handle, 11, 0)
 	time.sleep(1)
 
     # close handle (never reached in this case)
