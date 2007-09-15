d075ddc04fbf5bce0fba4e8f2770f89c0df433d8
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2007-09-15 12:30:55 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@702 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- at89prog/tool/at89prog.c
+++ at89prog/tool/at89prog.c
@@ -1,29 +1,11 @@
 /* 
  * Benedikt Sauter <sauter@ixbat.de> 2006-04-10
- *
+ * GNU/GPL 2
  *  Using:
  *  at89prog -t at89 -r	  // reset 
  *  at89prog -t at89 -e	  // erase
  *  at89prog -t at89 -u test.bin // upload 
  *
- *  Types:
- *  at89 	- AT89...
- *  m8		- atmega8
- *	m32		- atmega32
- *
- *	Message to at89prog Hardware:
- *
- * 	type cmd 
- * 	
- *	upload:
- * 	type cmd startaddrhigh staraddrlow len 
- *
- *	erase:
- *	type cmd
- * 
- *  reset:
- *	type cmd
-
  */
 
 #include <string.h>
@@ -39,12 +21,6 @@
 #define BUF 4096
 
 
-#define AT89	0x01
-#define M8		0x02	// atmega8
-#define M32		0x03	// atmega32
-
-
-
 #define RESET 0x00
 #define ERASE 0x01
 #define UPLOAD 0x02
@@ -56,86 +32,89 @@ void at89_reset(usb_dev_handle * usb_handle,char type);
 void at89_erase(usb_dev_handle * usb_handle,char type);
 void at89_upload(usb_dev_handle * usb_handle,char type,char *file);
 
-void atmega_reset(usb_dev_handle * usb_handle,char type);
-void atmega_erase(usb_dev_handle * usb_handle,char type);
-void atmega_upload(usb_dev_handle * usb_handle,char type,char *file);
-
 
 void show_help(void) {
-	printf("\nAuthor: Benedikt Sauter <sauter@ixbat.de>"\
-		"\nLicense: GNU General Public License V2\n"\
-		"\nat89prog is software for programming Microcontrollers.\n"\
-   		"\nUsage: at89prog [OPTION]\n"\
-       	"\t-r  reset controller\n"\
-       	"\t-e  erase code memory\n"\
-       	"\t-u  load .bin into code memory\n "\
-       	"\t-h  Show this help message\n"
-       	"\t-v  version\n\n");
+  printf("\nAuthor: Benedikt Sauter <sauter@ixbat.de>"\
+    "\nLicense: GNU General Public License V2\n"\
+    "\nat89prog is software for programming Microcontrollers.\n"\
+    "\nUsage: at89prog [OPTION]\n"\
+    "\t-r  reset controller\n"\
+    "\t-e  erase code memory\n"\
+    "\t-u  load .bin into code memory\n "\
+    "\t-h  Show this help message\n"
+    "\t-v  version\n\n");
 }
 
 
 int getoptown(char *argument, char *option) {
-   if( argument[0]=='-' && argument[1]==option[0] )
-      return 1;
-   return 0;
+  if( argument[0]=='-' && argument[1]==option[0] )
+    return 1;
+  return 0;
 }
 
 int main (int argc,char **argv)
 {
-  	struct usb_dev_handle *usb_handle;
-  	struct usb_device *usb_device;
-  	int send_status;
-  	long erg,port,pin,value;
-  	unsigned char send_data[4];
-  	unsigned char receive_data=0;
+  struct usb_dev_handle *usb_handle;
+  struct usb_device *usb_device;
+  int send_status;
+  long erg,port,pin,value;
+  unsigned char send_data[4];
+  unsigned char receive_data=0;
+  int error;
 
 
-  	usb_init();
-  	//usb_set_debug(2);
-  	if ((usb_handle = locate_at89prog())) {
-   		at89prog_open(usb_handle);
-  	}  
-	else {
-    	fprintf(stderr,"Could not open at89prog\n");
-	}
+  usb_init();
+
+  if ((usb_handle = locate_at89prog())) {
+    at89prog_open(usb_handle);
+    error = 0;
+  }  
+  else {
+    fprintf(stderr,"\nCould not open at89prog usb device!\n\n");
+    error = 1;
+  }
 
 
-   int counter=3;
-   char buffer[BUF];
-   size_t len=0;
+  int counter=3;
+  char buffer[BUF];
+  size_t len=0;
 
-   if(argc == 1 || getoptown(argv[1],"h") == TRUE ) {
-      show_help();
+  if(argc == 1 || getoptown(argv[1],"h") == TRUE ) {
+    show_help();
+    return EXIT_FAILURE;
+  }
+
+
+  else if(getoptown(argv[1],"v") == TRUE) {
+    printf("Version 1.0\n");
+    return EXIT_SUCCESS;
+  }
+  else if(argc < 2) {
+    show_help();
+    return EXIT_FAILURE;
+  }
+  if(error>0)
+    exit(0);
+
+  if(getoptown(argv[1],"r") == TRUE)
+    at89_reset(usb_handle,0);
+  else if(getoptown(argv[1],"e") == TRUE)
+    at89_erase(usb_handle,0);
+  else if(getoptown(argv[1],"u") == TRUE)
+  {
+    if(argc < 3) {
+      printf("upload file missing\n");
       return EXIT_FAILURE;
-   }
-
-   else if(getoptown(argv[1],"v") == TRUE) {
-      printf("Version 1.0\n");
-      return EXIT_SUCCESS;
-   }
-   else if(argc < 2) {
-      show_help();
-      return EXIT_FAILURE;
-   }
-   if(getoptown(argv[1],"r") == TRUE)
-      at89_reset(usb_handle,0);
-   else if(getoptown(argv[1],"e") == TRUE)
-      at89_erase(usb_handle,0);
-   else if(getoptown(argv[1],"u") == TRUE)
-   {
-	if(argc < 3) {
-		printf("upload file missing\n");
-		return EXIT_FAILURE;
-	}
+    }
     at89_upload(usb_handle,0,argv[2]);
-   }
-   else
-      show_help();
-   return EXIT_SUCCESS;
+  }
+  else
+    show_help();
 
-
-  usb_close(usb_handle);
+  if(usb_handle)
+    usb_close(usb_handle);
  	
+  return EXIT_SUCCESS;
 }	
 
 
@@ -206,73 +185,6 @@ void at89_upload(usb_dev_handle * usb_handle,char type,char *file)
 
 }
 
-
-
-
-/******************************************/
-/* functions for atmega */
-
-void atmega_reset(usb_dev_handle * usb_handle,char type)
-{
-  char send[1];
-  send[0]=RESET;
-  printf("start reset\n");
-  usb_bulk_write(usb_handle,2,send,1,1);
-  printf("ready\n");
-}
-
-void atmega_erase(usb_dev_handle * usb_handle,char type)
-{
-  char send[1];
-  send[0]=ERASE;
-  printf("start erasure\n");
-  usb_bulk_write(usb_handle,2,send,1,1);
-  printf("ready\n");
-}
-
-void atmega_upload(usb_dev_handle * usb_handle,char type,char *file)
-{
-  char send[MAXPACKETSIZE]; 
-  FILE *fd;
-  int addr=0;
-  int index=4;
-  int len;
-
-  printf("start upload\n");
-  // cmd startaddrhigh staraddrlow len 
-  // split in 60 byte packages
-  send[0]=UPLOAD;
-
-  fd = fopen(file, "r");
-  if(!fd) {
-    fprintf(stderr, "Unable to open file %s, ignoring.\n", file);
-  }
- 
-  while(!feof(fd))
-  { 
-    len = 0;
-    while(!feof(fd))
-    {
-      send[index++]=fgetc(fd);
-      if(index==(MAXPACKETSIZE-1))
-	break;
-    }
-    len = index-4;
-
-    send[1]=(char)(addr>>8);
-    send[2]=(char)addr;
-    send[3]=(char)len;
-    usb_bulk_write(usb_handle,2,send,MAXPACKETSIZE,1000);
-    addr = addr+len;
-
-    index=4;
-  }
-
-  fclose(fd);
- 
-  printf("ready\n");
-
-}
 
 
 
