dbf6fc2396bb8a77e2f509a3f4fa947642bcc6fa
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2007-11-17 09:27:45 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@837 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- usbprogWAV/tool/wavegen.py
+++ usbprogWAV/tool/wavegen.py
@@ -11,12 +11,12 @@ wavefile = wave.open('test.wav','w')
 
 wavefile.setnchannels(1)
 wavefile.setsampwidth(1)
-wavefile.setframerate(1000)
-wavefile.setnframes(1000)
+wavefile.setframerate(44000)
 
-for i in range(1,1000):
-    #data=struct.pack("h",int(math.sin(i) * 10))
-    data=struct.pack("h",i % 255)
+for i in range(1,44000):
+    #data=struct.pack("h",int(math.sqrt(math.sin(i)*math.sin(i))*100))
+    #data=struct.pack("B",int((math.sin(i)+1)*110))
+    data=struct.pack("b",i % 120)
     wavefile.writeframesraw(data)
 
 
