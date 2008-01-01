5e834c720c94b18f0cc5b4157eaa2121303b6bd8
flopper <flopper@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2008-01-01 16:15:44 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@903 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- usbprogLogic/firmware/main.c
+++ usbprogLogic/firmware/main.c
@@ -9,14 +9,12 @@
 #include "../../usbprog_base/firmwarelib/avrupdate.h"
 
 
-#include "ring.h"
+#define LED_PIN     PA4
+#define LED_PORT    PORTA
 
-#include "logic.h"
 
-
-SIGNAL(SIG_UART_RECV)
-{
-}
+#define LED_on     (LED_PORT   |=  (1 << LED_PIN))   // red led
+#define LED_off    (LED_PORT   &= ~(1 << LED_PIN))
 
 
 SIGNAL(SIG_INTERRUPT0)
@@ -24,47 +22,16 @@ SIGNAL(SIG_INTERRUPT0)
   USBNInterrupt();
 }
 
+void USBNDecodeVendorRequest(DeviceRequest *req)
+{
+  if(req->bRequest == STARTAVRUPDATE)
+      avrupdate_start();
+}
+
+volatile int togl;
 
 SIGNAL(SIG_OUTPUT_COMPARE1A)
 {
-  // activate signal for next measure
-  uint8_t port=PINB; 
-  int old,new;
-  if(logic.trigger!=TRIGGER_OFF)
-  {
-    if(logic.trigger==TRIGGER_EDGE)
-    {
-      old = logic.trigger_last & (1<<(logic.trigger_channel-1)) ?1:0;
-      new = port & (1<<(logic.trigger_channel-1)) ?1:0;
-      
-      if(logic.trigger_value==1)
-      {
-	if(new==1 && old==0)
-	  logic.trigger=TRIGGER_OFF;
-
-      }else
-      {
-	if(new==0 && old==1)
-	  logic.trigger=TRIGGER_OFF;
-      }
-	
-      logic.trigger_last=port;
-    }
-    if(logic.trigger==TRIGGER_PATTERN)
-    {
-      // ignore pattern
-      if( (port | logic.trigger_ignore)  == (logic.trigger_value|logic.trigger_ignore))
-      {
-	logic.trigger=TRIGGER_OFF;
-      }
-    }
-
-  }
-  else
-  {
-    ring_put (&logic.ring, port);
-  }
-  
   if(togl==1)
   {
     PORTA = 0xFF;
@@ -76,33 +43,36 @@ SIGNAL(SIG_OUTPUT_COMPARE1A)
   }
 }
 
-void USBNDecodeVendorRequest(DeviceRequest *req)
+void Commands(char * buf)
 {
-  switch(req->bRequest)
-  {
-    case STARTAVRUPDATE:
-      avrupdate_start();
-    break;
-  }
+
+
 }
+
 
 int main(void)
 {
   int conf, interf;
-  UARTInit();
+  
+  //UARTInit();
+  
   USBNInit();   
+
+  DDRA = (1 << PA4); // status led
+
+
   // setup your usbn device
 
-  USBNDeviceVendorID(0x0400);
-  USBNDeviceProductID(0x9876);
-  //USBNDeviceBCDDevice(0x0201);
+  USBNDeviceVendorID(0x1786);
+  USBNDeviceProductID(0x0c62);
+  USBNDeviceBCDDevice(0x0007);
 
 
   char lang[]={0x09,0x04};
   _USBNAddStringDescriptor(lang); // language descriptor
   
   USBNDeviceManufacture ("EmbeddedProjects");
-  USBNDeviceProduct	("usbprogLogic Analyzer 5V");
+  USBNDeviceProduct	("usbprogLogic 250");
 
   conf = USBNAddConfiguration();
 
@@ -111,79 +81,22 @@ int main(void)
   interf = USBNAddInterface(conf,0);
   USBNAlternateSetting(conf,interf,0);
 
-  
-  USBNAddInEndpoint(conf,interf,1,0x02,BULK,64,0,&LogicPingPongTX1); // scope data
-  USBNAddOutEndpoint(conf,interf,1,0x02,BULK,64,0,&LogicCommand); // scope commands
+  USBNAddInEndpoint(conf,interf,1,0x02,BULK,64,0,NULL);
+  USBNAddOutEndpoint(conf,interf,1,0x02,BULK,64,0,&Commands);
 
   
   USBNInitMC();
   sei();
-
-  // init ring
-  ring_init(&logic.ring, ringbuffer, 1000);   
-
-
-  //setup logic state and mode
-  logic.state=STATE_DONOTHING;
-  logic.mode=MODE_NONE;
-  logic.samplerate=SAMPLERATE_1MS;
-  logic.trigger=TRIGGER_OFF;
-  logic.trigger_last=0xff;
-
-  // start usb chip
   USBNStart();
 
-  //DDRB=0xff;
-  //DDRA=0xff;
-  DDRB=0x00; //in port
-  PORTB = 0xff; //internal pull up resistors
+  DDRA=0xff; // testing
+  //LED_on;
 
-  int datatogl=0;
+  //DDRB=0x00; //in port
+  //PORTB = 0xff; //internal pull up resistors
 
-  int ringstate=1;
-  int internstate=1;
-  int i,j;
-  while(1){
+  //int datatogl=0;
 
-    if(logic.ring.count>=1000)
-    {
-	if(logic.mode==MODE_LOGICINTERN)
-	{
-	  logic.state=STATE_DONOTHING;
-          TCCR1B = (1 << WGM12) | (0 << CS12) | (0 << CS11) | (0 << CS10);
-	    if(internstate)
-	    {
-	      UARTWrite("intern stoped");
-	      internstate=0;
-	    }
-	  logic.mode=MODE_LOGIC;
-	}
-    }
-
-  if(logic.ring.count>0 &&logic.tx==1 && logic.mode==MODE_LOGIC)
-  {
-    logic.tx=0;
-    USBNWrite(TXC1,FLUSH);
-
-    USBNWrite(TXD1,ring_get_nowait(&logic.ring));
-
-    if(logic.ring.count<63)
-      j=logic.ring.count;
-    else j=63;
-    for(i=0;i<j;i++)
-      USBNBurstWrite(ring_get_nowait(&logic.ring));
-
-  
-    if(datatogl==1)
-    {
-      USBNWrite(TXC1,TX_LAST+TX_EN+TX_TOGL);
-      datatogl=0;
-    }else
-    {
-      USBNWrite(TXC1,TX_LAST+TX_EN);
-      datatogl=1;
-    }
-  }
-  }
+  while(1);
 }
 
