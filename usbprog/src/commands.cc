7b4f8a1663b95b222a498d647c224668f0e81ba7
bwalle <bwalle@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2008-07-03 20:48:43 +0000

    - fix compiler warning: commands.cc:997: warning: control reaches end   of non-void function
    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@1039 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- usbprog/src/commands.cc
+++ usbprog/src/commands.cc
@@ -994,6 +994,8 @@ bool CopyingCommand::execute(CommandArgVector   args,
        << "GNU General Public License for more details.\n\n"
        << "You should have received a copy of the GNU General Public License\n"
        << "along with this program. If not, see <http://www.gnu.org/licenses/>.\n";
+
+   return true;
 }
 
 /* -------------------------------------------------------------------------- */
