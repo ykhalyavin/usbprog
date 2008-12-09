7997beb2b9fd18ba3683b7afbf0152fbe74275c8
memleak <memleak@70244ea8-9721-0410-aa8e-e1a1530c7cc1>
2008-12-09 11:41:13 +0000

    minor changes:  * Makefile:  sleep, so there's time for usbprog being recognized  * upload.pl: bugfix
    git-svn-id: http://svn.berlios.de/svnroot/repos/usbprog/trunk@1073 70244ea8-9721-0410-aa8e-e1a1530c7cc1

--- usbprog_base/firmware/upload.pl
+++ usbprog_base/firmware/upload.pl
@@ -9,7 +9,7 @@ sleep 0.5;
 my $cmd = `usbprog devices`;
 
 if($cmd =~ /\[\s*(\d+)\](.*?)c620$/m) {
-  system("usbprog device $? upload ../../avrispmk2klon/avrispmk2.bin");
+  system("usbprog device $1 upload ../../avrispmk2klon/avrispmk2.bin");
   print "Ok."
 } else {
   exit 1;
